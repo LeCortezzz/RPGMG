ef62d0b441db34af88046004e86adb6a817e6b2b
Victor <victor.marco2002@gmail.com>
2025-09-24 21:08:19 -0300

    V0.2-PRE-PRE-ALPHA - (external CSS and HTML inside PHP)

--- processa.php
+++ processa.php
@@ -4,12 +4,6 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $class = $_POST["class"];
     $alignment = $_POST["alignment"];
 
-    echo"<p>Você escolheu: </p>";
-    echo "<ul>";
-    echo "<li>Raça: $race</li>";
-    echo "<li>Classe: $class</li>";
-    echo "<li>Alinhamento: $alignment</li>";
-    echo "</ul>";
 
     $musics = [
         "Elfo"=> ["Elfo Musica 1","Elfo Musica 2", "Elfo Musica 3"],
@@ -18,33 +12,49 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         
         "Barbaro"=> ["Barbaro Musica 1","Barbaro Musica 2","Barbaro Musica 3"],
         "Mago"=> ["Mago Musica 1","Mago Musica 2","Mago Musica 3"],
-        "Guerreiro"=> ["Guerreiro Musica 1","Guerreiro Musica 2","Guerreiro Musica 3"],
+        "Arqueiro"=> ["Arqueiro Musica 1","Arqueiro Musica 2","Arqueiro Musica 3"],
         
         "Bom"=> ["Bom Musica 1","Bom Musica 2","Bom Musica 3"],
         "Neutro"=> ["Neutro Musica 1","Neutro Musica 2","Neutro Musica 3"],
         "Mau"=> ["Mau Musica 1","Mau Musica 2","Mau Musica 3"],
     ];
-
-    echo "<h2>Músicas Sugeridas:</h2>";
-    echo "<ul>";
-
-    if (isset($musics[$race])) {
-        foreach ($musics[$race] as $musicdrop) {
-            echo "<li>$musicdrop</li>";
-
-        }
-    }
-    if (isset($musics[$alignment])) {
-        foreach ($musics[$alignment] as $musicdrop) {
-            echo "<li>$musicdrop</li>";
-
-        }
-    }
-    if (isset($musics[$class])) {
-        foreach ($musics[$class] as $musicdrop) {
-            echo "<li>$musicdrop</li>";
-
-        }
-    }
-}
+} 
 ?>
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="style.css">
+        <title>Result</title>
+    </head>
+    <body>
+        <div class="containerIndex">
+            <h1>Você escolheu:</h1>
+                <ul>
+                    <li>Raça: <?= $race ?></li>
+                    <li>Classe: <?= $class ?></li>
+                    <li>Alinhamento: <?= $alignment ?></li>
+                </ul>
+            <h2>Músicas Sugeridas:</h2>
+                <ul>
+                    <?php if (isset($musics[$race])): ?>
+                        <?php foreach ($musics[$race] as $musicdrop): ?>
+                            <li><?= $musicdrop ?></li>
+                        <?php endforeach; ?>
+                    <?php endif; ?>
+                    <?php if (isset($musics[$class])): ?>
+                        <?php foreach ($musics[$class] as $musicdrop): ?>
+                            <li><?= $musicdrop ?></li>
+                        <?php endforeach; ?>
+                    <?php endif; ?>
+                    <?php if (isset($musics[$alignment])): ?>
+                        <?php foreach ($musics[$alignment] as $musicdrop): ?>
+                            <li><?= $musicdrop ?></li>
+                        <?php endforeach; ?>
+                    <?php endif; ?>
+                </ul>
+
+        </div>
+    </body>
+    </html>
