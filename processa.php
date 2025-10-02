b7bba95a8e37480ecf4263eacf96ec927a669cba
Victor <victor.marco2002@gmail.com>
2025-10-01 22:12:19 -0300

    V0.5-PRE-PRE-ALPHA (merging and slicing the array, showing yt embed links, presenting musics defined by array but still not properly)

--- processa.php
+++ processa.php
@@ -6,49 +6,65 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
 
     $raceGenres =[
-        "Elfo" = ["celtic","new-age","harp"]
-        "Humano" = ["soft-rock","progressive-rock","gregorian"]
-        "Goblin" = ["punk-rock","folk-punk","goblin-core"]
+        "Elfo" => ["celtic","new-age","harp"],
+        "Humano" => ["soft-rock","progressive-rock","gregorian"],
+        "Goblin" => ["punk-rock","folk-punk","goblin-core"]
     ];
     $classGenres =[
-        "Bárbaro" = ["metal","power-metal","doom"]
-        "Mago" = ["david-bowie","alternative","indie-rock"]
-        "Arqueiro" = ["ambient","rhythmic","celtic"]
+        "Barbaro" => ["metal","power-metal","doom"],
+        "Mago" => ["david-bowie","alternative","indie-rock"],
+        "Arqueiro" => ["ambient","rhythmic","celtic"]
     ];
     $alignmentGenres =[
-        "Bom" = ["hope","fast-paced","happy"]
-        "Neutral" = ["calm","passive","neutral"]
-        "Evil" = ["dark","evil","obscure"]
+        "Bom" => ["hope","fast-paced","happy"],
+        "Neutro" => ["calm","passive","neutral"],
+        "Mau" => ["dark","evil","obscure"]
     ];
-    $genresVideos = [
-        "celtic" = ["link1","link2","link3"]
-        "new-age" = ["link1","link2","link3"]
-        "harp" = ["link1","link2","link3"]
-        "soft-rock" = ["link1","link2","link3"]
-        "progressive-rock" = ["link1","link2","link3"]
-        "gregorian" = ["link1","link2","link3"]
-        "punk-rock" = ["link1","link2","link3"]
-        "folk-punk" = ["link1","link2","link3"]
-        "goblin-core" = ["link1","link2","link3"]
-        "metal" = ["link1","link2","link3"]
-        "power-metal" = ["link1","link2","link3"]
-        "doom" = ["link1","link2","link3"]
-        "david-bowie" = ["link1","link2","link3"]
-        "alternative" = ["link1","link2","link3"]
-        "indie-rock" = ["link1","link2","link3"]
-        "ambient" = ["link1","link2","link3"]
-        "rhythmic" = ["link1","link2","link3"]
-        "hope" = ["link1","link2","link3"]
-        "fast-paced" = ["link1","link2","link3"]
-        "happy" = ["link1","link2","link3"]
-        "calm" = ["link1","link2","link3"]
-        "passive" = ["link1","link2","link3"]
-        "neutral" = ["link1","link2","link3"]
-        "dark" = ["link1","link2","link3"]
-        "evil" = ["link1","link2","link3"]
-        "obscure" = ["link1","link2","link3"]
-    ]
+    $genresVideos =[
+        "celtic" => ["QRg_8NNPTD8","zNnzufK0nVE","zOvsyamoEDg"],
+        "new-age" => ["Jl8iYAo90pE","QllvHZW33dE","LTrk4X9ACtw"],
+        "harp" => ["vVTPbLhlbfI","BcElotwrZ8o","oPmKRtWta4E"],
+        "soft-rock" => ["ufQUxoidxkM","UrIiLvg58SY","dTDuq0rBplg"],
+        "progressive-rock" => ["IyUXTl_PQJ0","UgL-Pvuklcc","MjwttGizNEs"],
+        "gregorian" => ["sQNJs89E6x8","c38tWxPeCNM","o81A31hlgEA"],
+        "punk-rock" => ["JevbK84LxT4","vvWEKhGU6X4","hmxdNIyE4qI"],
+        "folk-punk" => ["4hHVdicce7w","mr5pyAGrnE","ogzlmuH7nuY"],
+        "goblin-core" => ["dQw4w9WgXcQ","dQw4w9WgXcQ","dQw4w9WgXcQ"],
+        "metal" => ["E0ozmU9cJDg","cEyiM9adE","5abamRO41fE"],
+        "power-metal" => ["0jgrCKhxE1s","zAaXhF9Fc-k","oOuSyKsZQ"],
+        "doom" => ["Z-71-i1akB0","AX9pqyjYZBM","N-gbPDh-G1Y"],
+        "david-bowie" => ["t365MuktYQs","kszLwBaC4Sw","iYYRH4apXDo"],
+        "alternative" => ["3mbBbFH9fAg","D0meVJwBUl0","EkG42gUyhMs"],
+        "indie-rock" => ["2SUwOgmvzK4","eCqdiwxMy4k","GL2UDFOg5f4"],
+        "ambient" => ["FEawZWIY1d4","ddZKwg63bRg","fTiyi0q37io"],
+        "rhythmic" => ["0cebiJV8tbs","ISCdNerKsi8","EzugmxtSqlo"],
+        "hope" => ["dQw4w9WgXcQ","dQw4w9WgXcQ","dQw4w9WgXcQ"],
+        "fast-paced" => ["dQw4w9WgXcQ","dQw4w9WgXcQ","dQw4w9WgXcQ"],
+        "happy" => ["dQw4w9WgXcQ","dQw4w9WgXcQ","dQw4w9WgXcQ"],
+        "calm" => ["dQw4w9WgXcQ","dQw4w9WgXcQ","dQw4w9WgXcQ"],
+        "passive" => ["dQw4w9WgXcQ","dQw4w9WgXcQ","dQw4w9WgXcQ"],
+        "neutral" => ["dQw4w9WgXcQ","dQw4w9WgXcQ","dQw4w9WgXcQ"],
+        "dark" => ["dQw4w9WgXcQ","dQw4w9WgXcQ","dQw4w9WgXcQ"],
+        "evil" => ["dQw4w9WgXcQ","dQw4w9WgXcQ","dQw4w9WgXcQ"],
+        "obscure" => ["dQw4w9WgXcQ","dQw4w9WgXcQ","dQw4w9WgXcQ"]
+    ];
 
+    $selectedGenres = array_merge(
+        $raceGenres[$race],
+        $classGenres[$class],
+        $alignmentGenres[$alignment],    
+    );
+
+    $selectedVideos = [];
+    foreach ($selectedGenres as $genre){
+        if(isset($genresVideos[$genre])){
+            $selectedVideos[] = $genresVideos[$genre][array_rand($genresVideos[$genre])];
+            
+
+        }
+
+    }
+    $selectedVideos = array_slice($selectedVideos, 0, 3);
 } 
 ?>
     <!DOCTYPE html>
@@ -71,21 +87,17 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
                     <li>Alinhamento: <?= $alignment ?></li>
                 </ul>
             <h2>Músicas Sugeridas:</h2>
-                <ul>
-                    <?php if (isset($musics[$race])): ?>
-                        <?php $key = array_rand($musics[$race]); ?>
-                            <li><?= $musics[$race][$key] ?></li>
-                    <?php endif; ?>
-                    <?php if (isset($musics[$class])): ?>
-                        <?php $key = array_rand($musics[$class]); ?>
-                            <li><?= $musics[$class][$key] ?></li>
-                    <?php endif; ?>
-                    <?php if (isset($musics[$alignment])): ?>
-                        <?php $key = array_rand($musics[$alignment]); ?>
-                            <li><?= $musics[$alignment][$key] ?></li>
-                    <?php endif; ?>
-                    
-                </ul>
+                <?php if (!empty($selectedVideos)): ?>
+            <div class="videos-container">
+            <?php foreach ($selectedVideos as $videoId): ?>
+                <iframe width="auto" height="auto"
+                        src="https://www.youtube.com/embed/<?= $videoId ?>" 
+                        frameborder="0" allowfullscreen></iframe>
+            <?php endforeach; ?>
+            </div>
+        <?php else: ?>
+            <p>Nenhum vídeo encontrado para os gêneros selecionados.</p>
+        <?php endif; ?>
         </div>
     </body>
     </html>
